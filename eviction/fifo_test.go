edb829518f7eea800d63831c9580b6e62e55986a
devkarim <devkarim@hotmail.com>
2026-01-25 16:36:26 +0200

    feat: add test for fifo

---
+++ eviction/fifo_test.go
@@ -0,0 +1,50 @@
+package eviction
+
+import "testing"
+
+func TestFIFO_SelectVictim_Order(t *testing.T) {
+	fifo := NewFIFO()
+	fifo.Access("a")
+	fifo.Access("b")
+	fifo.Access("c")
+
+	result, ok := fifo.SelectVictim()
+	expected := "a"
+
+	if !ok || result != expected {
+		t.Errorf("expected %s, got %s", expected, result)
+	}
+
+	result, ok = fifo.SelectVictim()
+	expected = "b"
+
+	if !ok || result != expected {
+		t.Errorf("expected %s, got %s", expected, result)
+	}
+}
+
+func TestFIFO_SelectVictim_Empty(t *testing.T) {
+	fifo := NewFIFO()
+
+	_, ok := fifo.SelectVictim()
+	expected := false
+
+	if expected != ok {
+		t.Errorf("expected ok to be %t, got %t", expected, ok)
+	}
+}
+
+func TestFIFO_SelectVictim_Remove(t *testing.T) {
+	fifo := NewFIFO()
+	fifo.Access("a")
+	fifo.Access("b")
+	fifo.Remove("a")
+
+	result, ok := fifo.SelectVictim()
+	expected := "b"
+
+	if !ok || expected != result {
+		t.Errorf("expected ok to be %s, got %s", expected, result)
+	}
+}
+

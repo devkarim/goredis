b13e1260580388bc73a3e8c9596212f23b3747aa
devkarim <devkarim@hotmail.com>
2026-01-25 16:46:18 +0200

    test: add testing for eviction

--- storage/cache_test.go
+++ storage/cache_test.go
@@ -2,19 +2,21 @@ package storage
 
 import (
 	"testing"
+
+	"github.com/devkarim/goredis/eviction"
 )
 
-func TestStringSize(t *testing.T) {
+func TestRedisObject_StringSize(t *testing.T) {
 	obj := &RedisObject{Str: "Hello", Type: RedisObjectString}
 	result := obj.Size()
 	expected := 5
 
-	if  result != expected {
+	if result != expected {
 		t.Errorf("obj.Size() = %d; want %d", result, expected)
 	}
 }
 
-func TestHashSize(t *testing.T) {
+func TestRedisObject_HashSize(t *testing.T) {
 	hash := make(map[string]string)
 	hash["u1"] = "karim"
 	hash["u2"] = "ahmed"
@@ -23,7 +25,24 @@ func TestHashSize(t *testing.T) {
 	result := obj.Size()
 	expected := 14
 
-	if  result != expected {
+	if result != expected {
 		t.Errorf("obj.Size() = %d; want %d", result, expected)
+	}
+}
+
+func TestEviction_FIFO(t *testing.T) {
+	policy := eviction.NewFIFO()
+	shard := &Shard{Store: make(map[string]*RedisObject), Policy: policy, MaxMemory: 10}
+
+	shard.SetString("a", "12345")
+	shard.SetString("b", "12345")
+	shard.SetString("c", "12345")
+
+	if _, ok := shard.Store["a"]; ok {
+		t.Errorf("'a' should have been evicted")
+	}
+
+	if _, ok := shard.Store["c"]; !ok {
+		t.Errorf("'c' should exist")
 	}
 }
